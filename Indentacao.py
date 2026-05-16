7458bca222a583ec8b078d9bf154db7140720609
Geraldo Humberto <geraldohum@gmail.com>
2026-05-16 13:27:12 -0300

    Arquivos do curso

---
+++ Indentacao.py
@@ -0,0 +1,13 @@
+if condition:
+
+    # Bloco de código se a condição for verdadeira
+
+    instrucao1
+    instrucao2
+
+else:
+
+    # Bloco de código se a condição for falsa
+
+    instrucao3
+    instrucao4
